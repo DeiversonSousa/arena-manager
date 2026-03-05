b0f6a4b87ea71cff648204d4b0c9978000326fde
Deiverson Sousa <deiverson.souza2k18@gmail.com>
2026-03-04 23:46:17 -0300

    firts commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+     var typed = new Typed('.words-wrappen', {
+      strings: ['FUTEBOL', 'CAMPEONATO'],
+      typeSpeed: 100,
+      startDelay: 0,
+      loop: true
+    });
+})
