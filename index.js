693fb666a62a8a857fcfbaf2381984160b2c21a1
Deiverson Sousa <deiverson.souza2k18@gmail.com>
2026-03-08 23:17:18 -0300

    Start Responsive

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// START NAMES BACKGROUND
 document.addEventListener('DOMContentLoaded', () => {
      var typed = new Typed('.words-wrappen', {
       strings: ['FUTEBOL', 'CAMPEONATO'],
@@ -5,4 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
       startDelay: 0,
       loop: true
     });
+})
+// END NAMES BACKGROUND
+
+// Rondom Images Background
+const imagens = [
+        "img/background.jpg",
+        "img/background-2.jpg",
+        "img/background-3.jpg",
+        "img/background-4.jpg"
+    ];
+
+let indice = 0;
+const divFundo = document.querySelector(".background-1");
+
+    function trocarBackground() {
+        divFundo.style.backgroundImage = `url('${imagens[indice]}')`;
+        indice = (indice + 1) % imagens.length;
+    }
+    
+
+    // Troca inicial
+    trocarBackground();
+
+    // Troca a cada 5 segundos
+    setInterval(trocarBackground, 15000);
+// END IMAGENS BACKGROUND
+
+// START RESPONSIVE
+
+var menuIcon = document.querySelector('.icone-menu');
+
+menuIcon.addEventListener('click', ()=>{
+    alert('itsa working')
 })
